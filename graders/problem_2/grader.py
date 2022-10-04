5f09fcf96bbd2918ff704d1291dff260d8de5d59
Harshit Gupta <harshit.11235@gmail.com>
2022-10-04 21:33:12 +0530

    complete writeup

---
+++ graders/problem_2/grader.py
@@ -0,0 +1,16 @@
+# simple graph problem
+
+INPUT_PATH = "tests/inputs/"
+
+
+class grader1:
+    @classmethod
+    def evaluate(cls, get_min_swaps_line):
+        # get min swaps lines is the users' function
+
+        # load your tests here
+        pass
+
+
+class grader2:
+    pass
